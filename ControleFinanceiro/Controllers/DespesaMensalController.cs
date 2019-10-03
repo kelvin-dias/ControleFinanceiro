8842fcfb8aa81305200305e41642dab98e5b3bb9
Kelvin Oliveira <kelvinodias@gmail.com>
2019-10-03 17:42:42 -0300

    Alteração na View Dashboard e Referências 2

--- ControleFinanceiro/Controllers/DespesaMensalController.cs
+++ ControleFinanceiro/Controllers/DespesaMensalController.cs
@@ -20,6 +20,11 @@ namespace ControleFinanceiro.Controllers
             return View(despesaMensalServico.ObterDespesasMensais());
         }
 
+        public ActionResult TabelaDespesasMensais()
+        {
+            return View(despesaMensalServico.ObterDespesasMensais());
+        }
+
         // GET: DespesaMensal
         public ActionResult AdicionarDespesaMensal()
         {
@@ -35,7 +40,7 @@ namespace ControleFinanceiro.Controllers
                 if (ModelState.IsValid)
                 {
                     despesaMensalServico.AdicionarDespesaMensal(despesaMensal);
-                    return RedirectToAction("Index");
+                    return RedirectToAction("TabelaDespesasMensais");
                 }
                 return View(despesaMensal);
             }
--- Modelo/Entidades/DespesaMensal.cs
+++ Modelo/Entidades/DespesaMensal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Modelo.Entidades
@@ -6,10 +7,12 @@ namespace Modelo.Entidades
     public class DespesaMensal
     {
         public int? DespesaMensalId { get; set; }
+        [DisplayName("Item")]
         public string NomeItem { get; set; }
-
+        [DisplayName("Valor do Item")]
         [DisplayFormat(DataFormatString = "{0,N2}")]
         public decimal Valor { get; set; }
+        [DisplayName("Data")]
         public DateTime DataHora { get; set; }
 
     }
